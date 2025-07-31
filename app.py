66e746a48e72e4aa70abd3af7047b825788e90ee
hongnhung93 <nguyenhongnhung2551993@gmail.com>
2025-07-31 16:37:29 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+app = Flask(__name__)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.json
+    user_input = data.get("message", "")
+    input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors="pt")
+    output_ids = model.generate(input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    response = tokenizer.decode(output_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
+    return jsonify({"response": response})
+
+if __name__ == "__main__":
+    app.run(debug=True)
